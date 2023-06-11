454ccb2a36067f561727cf454b2232921f255f60
kirill <kiruha3@mail.ru>
2023-06-11 13:28:00 +0700

    пятая задача

--- src/Main.java
+++ src/Main.java
@@ -67,5 +67,30 @@ public class Main {
     }
 
     private static void task5() {
+        int monthNumber = 5;
+        switch (monthNumber){
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+        default: break;
+
+        }
     }
 }
